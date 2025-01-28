eff80788b1cda21a44a09cc58a6ab383c1dcb9a7
GHOSTLXH <41314888+GHOSTLXH@users.noreply.github.com>
2025-01-28 22:04:44 +0800

    节点文件

--- IntervalCounter A node/interval_counter.py
+++ IntervalCounter A node/interval_counter.py
@@ -22,8 +22,6 @@ class IntervalCounter:
     FUNCTION = "counter"
     CATEGORY = "Custom Nodes/Counters"
 
- # Set IS_CHANGED to True to ensure the node is always considered dirty
-    IS_CHANGED = True
     
     def __init__(self):
         self.load_state()
