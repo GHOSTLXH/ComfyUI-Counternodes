65dabfb16af28b654400e4897bde4e1857216666
GHOSTLXH <41314888+GHOSTLXH@users.noreply.github.com>
2025-01-28 12:34:47 +0800

    Create __init__.py
    对应节点的映射文件

---
+++ alternating_output A node/__init__.py
@@ -0,0 +1,10 @@
+from .alternating_output import AlternatingOutput
+
+NODE_CLASS_MAPPINGS = {
+    "AlternatingOutput": AlternatingOutput
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AlternatingOutput": "Alternating Output"
+}
